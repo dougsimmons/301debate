4ada97af6f6052d1255bc5a7e7c39c46e62eee52
Doug Simmons <git@hmm.nyc>
2016-01-31 18:48:26 -0500

    beautified

--- 301debate.sh
+++ 301debate.sh
@@ -3,4 +3,7 @@
 # Thank you wangman from HN for showing me the light! https://news.ycombinator.com/item?id=11008186
 # Look at this loop, it's beautiful:
 echo wait for it...
-echo -e "sec\tmethod\turl";for url in {https,http}://{www.,}{en.wikipedia.org,{google,reddit,facebook,youtube,netflix,amazon,twitter,linkedin,msn}.com,google.co.in,hmm.nyc}; do curl -sL "$url" -w "%{time_redirect}\t${url%:}\t${url#//}\n" -o/dev/null; done|sort
+echo -e "sec\tmethod\turl";
+for url in {https,http}://{www.,}{en.wikipedia.org,{google,reddit,facebook,youtube,netflix,amazon,twitter,linkedin,msn}.com,google.co.in,hmm.nyc}; 
+  do curl -sL "$url" -w "%{time_redirect}\t${url%:}\t${url#//}\n" -o/dev/null; 
+done|sort
