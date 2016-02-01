b32993be23fee65fa2937d1ed68a818adbc3e5ed
Doug Simmons <git@hmm.nyc>
2016-02-01 10:03:49 -0500

    Update 301debate.sh

--- 301debate.sh
+++ 301debate.sh
@@ -1,6 +1,7 @@
 # bin bash slash baby!
 # 301debate.sh v0.4 by Doug Simmons / github@hmm.nyc
 # Thank you wangman from HN for showing me the light! https://news.ycombinator.com/item?id=11008186
+# Uses this command: curl -sL https://{www.,}hmm.nyc -o\ /dev/null{,\ } -w "%{time_redirect}\n" -o/dev/null{,}
 # Look at this loop, it's beautiful:
 echo wait for it...
 echo -e "sec\tmethod\turl";
